907e6cd4fa7cb6d6d6bcde5ca5013c56a628fef3
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-02-11 13:46:38 -0500

    cola/issue #2: build employee class and program tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,32 @@
+const Employee = require('../lib/Employee.js');
+
+test('creates new employee object', () => {
+  const employee = new Employee('Nicola', 1, 'marblenicola@gmail.com');
+
+  expect(employee.name).toBe('Nicola');
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toBe('marblenicola@gmail.com');
+})
+
+test("get's the employee's name", () => {
+  const employee = new Employee('Nicola', 1, 'marblenicola@gmail.com');
+
+  expect(employee.getName()).toEqual(expect.stringContaining(employee.name));
+})
+
+test("get's the employee's ID number", () => {
+  const employee = new Employee('Nicola', 1, 'marblenicola@gmail.com');
+
+  expect(employee.getId()).toEqual(expect.any(Number));
+})
+
+test("get the Employee's email", () => {
+  const employee = new Employee('Nicola', 1, 'marblenicola@gmail.com')
+  expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email));
+})
+
+test("get the Employee's role", () => {
+  const employee = new Employee('Nicola', 1, 'marblenicola@gmail.com')
+
+  expect(employee.getRole()).toBe('Employee');
+})
